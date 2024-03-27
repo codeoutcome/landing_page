d0984ba1945c224f53298d18ebc9670320bdd07d
Sam Stimpson <hello+code@samstimpson.com>
2024-03-27 17:06:55 +0000

    added the navigation, homepage banner content

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BsCart2 } from "react-icons/bs";
 import { HiOutlineBars3 } from "react-icons/hi2";
 import Box from "@mui/material/Box";
@@ -14,10 +14,77 @@ import InfoIcon from "@mui/icons-material/Info";
 import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
 import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
 import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
+import Logo from "../Assets/Logo.svg";
 
 export const Navbar = () => {
+
+  const [openMenu, setOpenMenu] = useState(false);
+  console.log(openMenu);
+
+  const menuOptions = [
+    {
+      text: 'Home',
+      icon: <HomeIcon/>,
+    },
+    {
+      text: 'About',
+      icon: <InfoIcon/>,
+    },
+    {
+      text: 'Testimonials',
+      icon: <CommentRoundedIcon/>,
+    },
+    {
+      text: 'Contact',
+      icon: <PhoneRoundedIcon/>,
+    },
+    {
+      text: 'Cart',
+      icon: <ShoppingCartRoundedIcon/>,
+    },
+  ]
+
   return (
-    <div>NavBar</div>
+    <nav>
+      <div className='nav-logo-container'>
+        <img src={Logo} alt='' />
+      </div>
+      <div className='navbar-links-container'>
+        <a href=''>Home</a>
+        <a href=''>About</a>
+        <a href=''>Testimonials</a>
+        <a href=''>Contact</a>
+        <a href=''>
+          <BsCart2 className='navbar-cart-icon'/>
+        </a>
+        <button className='primary-button'>
+          Book Now
+        </button>
+      </div>
+      <div className='navbar-menu-container'>
+        <HiOutlineBars3 open={openMenu} onClick={() => setOpenMenu(true)} />
+      </div>
+      <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right">
+        <Box
+          sx={{ width: 250 }}
+          role="presentation"
+          onClick={() => setOpenMenu(false)}
+          onKeyDown={() => setOpenMenu(false)}
+        >
+          <List>
+            {menuOptions.map((item) => (
+              <ListItem key={item.text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>{item.icon}</ListItemIcon>
+                  <ListItemText primary={item.text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+        </Box>
+      </Drawer>
+    </nav>
   )
 }
 
